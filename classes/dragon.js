284148b6460b44ebc810334938fc0a5b9225351f
gregorio <gregorio.franco1985@gmail.com>
2023-08-17 23:12:07 -0400

    2

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,6 +1,17 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
 
-/****************************************************************************/
+  breathesFire(name) {
+    console.log(`${name} breathes fire everywhere! BURN!!!!`);
+  }
+  static getDragons(...args) {
+    return [dragon.name];
+  }
+
+}
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
